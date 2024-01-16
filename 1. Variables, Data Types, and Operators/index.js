da33abfbcc72219369179ad4acbfbb53bfc3d926
micoescsr <123940221+micoescsr@users.noreply.github.com>
2024-01-16 13:04:39 +0000

    Exercise 1 - Done - Did some revisions

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -10,22 +10,29 @@
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
 // Your code here
 
+console.log("1.1")
 var x = 1
 console.log(x)
+console.log()
 
 
 // TODO 1.2 Use the let keyword to define a variable.
 // Your code here
 
+console.log("1.2")
 let name = "Mico"
 console.log(name)
+console.log()
 
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
 
+// REMOVE COMMENT TO SEE WHAT HAPPENS
+//console.log("1.3")
 // const num = 52
 // num = 55
 // console.log(num)
+// console.log()
 
 // Answer: When I reassigned the value of x from 52 to 55, it throwed an error because it is not possible to reassign
 //         the value of a const variable.
@@ -38,22 +45,28 @@ console.log(name)
 // TODO 1.4 Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
 // Your code here
+console.log("1.4")
 const y = 1 + 5 * 5 / 13
 console.log(y)
+console.log()
 
 // Checkpoint 1.2 What operators did you use?
 // Answer: Addition, Multiplication, and Division
 // Your code here
+console.log("Checkpoint 1.2")
 const g = 1 + 5 * 5 / 13
 console.log(g)
+console.log()
 
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
 // Your code here
 
+console.log("1.5")
 let str1 ="Mico"
 let str2 ="Efraim"
 console.log(str1+str2)
+console.log()
 
 // Checkpoint 1.3 What operators did you use?
 // Answer: Concatenation Operator
@@ -62,23 +75,28 @@ console.log(str1+str2)
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
 // Your code here
 
+console.log("1.6")
 const cont1 = true || false 
 console.log(cont1)
 const cont2 = false && true
 console.log(cont2)
+console.log()
 
 // Checkpoint 1.4 What operators did you use?
 // Answer: AND and OR
 
-
 // TODO 1.7 Try out the different relational operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#relational_operators
+
+console.log("1.7")
 const names = ["moshi","fiona","tricia","robert","cate","dion"]
 if (0 in names){
     console.log("MADZWAE KO YAN")
 }
+console.log()
 
 // Checkpoint 1.5 What does [] == false evaluate to? Why does that happen? Research why
 // Answer: [] is an object so any array that is empty will always result to false
 // Your code here
+console.log("1.8")
 console.log(![])
